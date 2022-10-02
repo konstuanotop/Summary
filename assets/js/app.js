3f86d93cc4a12944c133c1d4081282fc372283f7
Kostya Mamontov <konstantin-1997@mail.ru>
2022-10-02 23:04:16 +0600

    third commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,18 @@ $(function () {
         $(this).toggleClass("active");
         $("#nav").toggleClass("active");
     });
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        var blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+
+    });
+
 });
